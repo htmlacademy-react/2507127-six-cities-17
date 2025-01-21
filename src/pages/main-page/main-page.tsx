9b49fbcb9b7755b6fdd32c742aba86b8082e2ed9
Farit <afroid13@gmail.com>
2025-01-21 15:37:58 +0400

    fix: Сброс состояния activeOfferId пермещен в useEffect

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,18 +1,9 @@
 import Header from '../../components/header/header';
 import { PagesList } from '../../const';
 import Title from '../../components/title/title';
-import { useAppDispatch } from '../../hooks';
-import { useEffect } from 'react';
 import MainPageContent from '../../components/main-page-content/main-page-content';
-import { changeActiveOfferId } from '../../store/offers-process/offers-process.slice';
 
 function MainPage(): JSX.Element{
-  const dispatch = useAppDispatch();
-  //Сброс выбранного id. Ранее, при возврате со страницы offer, выбранный id отсавался в глобальном состоянии
-  useEffect(() =>{
-    dispatch(changeActiveOfferId(null));
-  });
-
   return (
     <div className="page page--gray page--main">
       <Title pageName={PagesList.Main}/>
